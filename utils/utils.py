19387056da18efdc99e960ecd88f9505e44bbfa2
Hugo Pradier <hugo.pradier66@gmail.com>
2021-05-28 12:25:41 +0200

    last commit wullah

--- utils/utils.py
+++ utils/utils.py
@@ -8,10 +8,10 @@ import time
 import json
 import jmespath as jp
 from typing import List, Dict, Tuple
+import csv
 
 class websc:
-    
-    #defining the attributes of each property
+
     locality : List[str] = []
     type_of_property : List[str]= []
     subtype_of_property : List[str] = []
@@ -37,56 +37,121 @@ class websc:
     properties_url : List[str] = []
     data : list = []
     temp_dict : dict = {}
-    df = pd.DataFrame()
-
-    def change_my_page(self, nbr):
+    df = list
+    my_df = pd.DataFrame()
+    timeStart = time.perf_counter()
+    property_nbr = 0
+    total_page_number = 333
+    total_property_number = 14769
+    
+    def change_my_page(self, nbr: int) -> None: #Function that generate new URL to go page to page
         self.page_number = nbr
-        self.search_url = (f"https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&isALifeAnnuitySale=false&propertySubtypes=BUNGALOW,CHALET,FARMHOUSE,CASTLE,COUNTRY_COTTAGE,EXCEPTIONAL_PROPERTY,APARTMENT_BLOCK,MIXED_USE_BUILDING,TOWN_HOUSE,MANSION,VILLA,OTHER_PROPERTY,MANOR_HOUSE,PAVILION,GROUND_FLOOR,DUPLEX,TRIPLEX,FLAT_STUDIO,PENTHOUSE,LOFT,SERVICE_FLAT,KOT&page={nbr}&orderBy=relevance")
+        self.search_url = (f"https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&propertySubtypes=BUNGALOW,CASTLE,COUNTRY_COTTAGE,TOWN_HOUSE,VILLA,MANOR_HOUSE,GROUND_FLOOR,TRIPLEX,PENTHOUSE,CHALET,FARMHOUSE,EXCEPTIONAL_PROPERTY,MANSION,PAVILION,DUPLEX,FLAT_STUDIO,LOFT,APARTMENT_BLOCK&page={nbr}&orderBy=relevance")
 
-    def crawl(self):
+    def crawl(self) -> None: #
         for i in self.data:
             for k,v  in enumerate(i):
                 if v == 'classified':
                     self.temp_dict = list(i.items())[k][1]
-                    self.parse_data(self.temp_dict)
-
+                    self.parse_data()
                     
-    def parse_data(self, new_dict):
-    #parsing json for reqd info
+    def parse_data(self) -> None: #Parse data from dictionnary
+    
+        try:
+            zip_code = jp.search('zip', self.temp_dict)
+        except IndexError:
+            zip_code = None
+        self.locality.append(zip_code)
+        
+        try:
+            type_property = jp.search('type', self.temp_dict)
+        except IndexError:
+            type_property = None
+        self.type_of_property.append(type_property)
 
         try:
-            rooms = jp.search('bedroom.count', new_dict)          
+            subtype_property = jp.search('subtype', self.temp_dict)
+        except IndexError:
+            subtype_property = None
+        self.subtype_of_property.append(subtype_property)
+        
+        try:
+            price_property = jp.search('price', self.temp_dict)
+        except IndexError:
+            price_property = None
+        self.price.append(price_property)
+
+        try:
+            type_sale = jp.search('transactionType', self.temp_dict)
+        except IndexError:
+            type_sale = None
+        self.type_of_sale.append(type_sale)
+        
+        try:
+            rooms = jp.search('bedroom.count', self.temp_dict)          
         except IndexError:
             rooms = None
         self.number_of_rooms.append(rooms)
 
         try:
-            state_of_the_bldg = jp.search('building.condition', new_dict)
-            #print('state = ',state_of_the_bldg)
+            area = jp.search('land.surface', self.temp_dict)
+        except IndexError:
+            area = None
+        self.area_in_sq_meters.append(area)
+
+        try:
+            kitchen = jp.search('kitchen.type', self.temp_dict)
+            kitchen = True
+        except IndexError:
+            kitchen = False
+        self.fully_equipped_kitchen.append(kitchen)
+
+        try:
+            trc = jp.search('outdoor.terrace.exists', self.temp_dict)
+            if trc == 'true': trc = True
+        except IndexError:
+            trc = False
+        self.terrace.append(trc)
+
+        try:
+            grdn = jp.search('outdoor.garden.exists', self.temp_dict)
+            if grdn == 'true': grdn = True
+        except IndexError:
+            grdn = False
+        self.garden.append(grdn)
+         
+        try:
+            swim_pool= jp.search('wellnessEquipment.hasSwimmingPool.type', self.temp_dict)
+            if swim_pool == 'true': swim_pool = True
+        except IndexError:
+            swim_pool = False
+        self.swimming_pool.append(swim_pool)
+
+        try:
+            state_of_the_bldg = jp.search('building.condition', self.temp_dict)
         except IndexError:
             state_of_the_bldg = None
         self.state_of_the_building.append(state_of_the_bldg)
 
-        try:
-            kitchen= jp.search('classified.kitchen.type', new_dict)
-            #print('kitchen = ',kitchen)
-        except IndexError:
-            kitchen = None
-        self.fully_equipped_kitchen.append(kitchen)
+       
 
-        #making a list of lists    
-        self.df_list.append(pd.DataFrame({'Rooms': self.number_of_rooms,
-                            'State of building': self.state_of_the_building,
-                            'Fully equipped kitchen': self.fully_equipped_kitchen},
-                             index = [[0]]))
+    def create_csv(self) -> None: #Will save the csv on sys
+        self.df = [self.locality, self.type_of_property,
+                    self.subtype_of_property, self.price,
+                    self.number_of_rooms, self.area_in_sq_meters,
+                    self.fully_equipped_kitchen, self.terrace,
+                    self.garden, self.swimming_pool,
+                    self.state_of_the_building]
 
-        self.create_data_frame()
-        
-
-    def create_data_frame(self):
-        
-        #adding to data frame
-        self.df = pd.concat(self.df_list, ignore_index=True, sort=False)
-        print(self.df)
-
-    
+        self.my_df = pd.DataFrame(self.df)
+        self.my_df = self.my_df.transpose()
+        self.my_df.to_csv(r'./belgian_housing_data.csv',index=False,
+                         header = ["locality","type_of_property",
+                            "subtype_of_property","price","number_of_rooms",
+                            "area_in_sq_meters","fully_equipped_kitchen",
+                            "terrace","garden","swimming_pool",
+                            "state_of_the_building"])
+        print(self.my_df.shape)
+        # with open("../export_dataframe.csv", "w", newline="") as f:
+        #     writer = csv.writer(f)
+        #     writer.writerows(self.df)
--- web-s.py
+++ web-s.py
@@ -3,41 +3,34 @@ from utils.utils import websc
 import time
 
 websc = websc()
-timeStart = time.perf_counter()
-property_nbr = 0
-#use google chrome webdriver
 driver = webdriver.Chrome("/usr/bin/chromedriver")
-total_page_number = 333
-total_property_number = 14769
-#
-#  Get all the URLS
-#
-for i in range(1, 2):
+
+for i in range(1, 333):
     websc.change_my_page(i)
     print(f"\n\n !!! \n\n NEW PAGE ==> {websc.search_url}\n  PAGE NUMBER ==> {websc.page_number}\n\n\n !!! \n\n ")
-    driver.get(websc.search_url) #url to search
+    driver.get(websc.search_url)
     time.sleep(1)
-    #print (websc.search_url)
+    print (websc.search_url)
     assert "Immoweb" in driver.title
     for elem in driver.find_elements_by_xpath("//a[@class='card__title-link']"): #liens des maisons
         websc.properties_url.append(elem.get_attribute("href"))
         websc.simple_property_URL = elem.get_attribute("href")
         timecount= time.perf_counter()
-        print(f'\n  NEW_URL_PROPERTY ==> {websc.simple_property_URL}\n  PROPERTY NUMBER ==> {property_nbr} / {total_property_number}     Time : {timecount - timeStart:f} seconds\n  PAGE NUMBER ==> {websc.page_number} / {total_page_number}\n')
-        property_nbr += 1
-property_nbr = 1
-#
-#  Get data from every url
-#
+        print(f'\n  NEW_URL_PROPERTY ==> {websc.simple_property_URL}\n  PROPERTY NUMBER ==> {websc.property_nbr} / {websc.total_property_number}     Time : {timecount - websc.timeStart:f} seconds\n  PAGE NUMBER ==> {websc.page_number} / {websc.total_page_number}\n')
+        websc.property_nbr += 1
+websc.property_nbr = 1
+
 for url in websc.properties_url:
     timecount = time.perf_counter()
     driver.get(url)
     time.sleep(1)
     websc.data = driver.execute_script("return window.dataLayer")
-    print(f'\n  PROPERTY_SCRAP ==> {url}\n  PROPERTY NUMBER ==> {property_nbr} / {total_property_number}    Time : {timecount - timeStart:f} seconds\n  PAGE NUMBER ==> {round (property_nbr / 30)} / {total_page_number}\n')
-    property_nbr += 1
+    print(f'\n  PROPERTY_SCRAP ==> {url}\n  PROPERTY NUMBER ==> {websc.property_nbr} / {websc.total_property_number}    Time : {timecount - websc.timeStart:f} seconds\n  PAGE NUMBER ==> {round (websc.property_nbr / 30)} / {websc.total_page_number}\n')
+    websc.property_nbr += 1
     websc.crawl()
 
+
+websc.create_csv()
 driver.close()
 
 print("\n\n§§§§§DONE§§§§§")
